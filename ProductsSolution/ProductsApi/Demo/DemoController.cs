27005a096fd837c961779b6d9d537d8b16c3a454
Jeffry Gonzalez <jeff@hypertheory.com>
2023-05-01 15:48:27 -0400

    test projects created

---
+++ ProductsSolution/ProductsApi/Demo/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductsApi.Demo;
+
+public class DemoController : ControllerBase
+{
+    [HttpGet("/demo")]
+    public async Task<ActionResult> GetTheDemo()
+    {
+        return Ok();
+    }
+}
