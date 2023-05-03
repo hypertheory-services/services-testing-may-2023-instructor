6973c474c97b49f7dcaa33093096748e634a1757
Jeffry Gonzalez <jeff@hypertheory.com>
2023-05-03 12:04:23 -0400

    first database test

---
+++ ProductsSolution/ProductsApi.IntegrationTests/Products/Fixtures/ProductsDatabaseFixture.cs
@@ -0,0 +1,49 @@
+﻿
+using Alba;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Builders;
+using Testcontainers.PostgreSql;
+using Marten;
+
+namespace ProductsApi.IntegrationTests.Products.Fixtures;
+
+public class ProductsDatabaseFixture : IAsyncLifetime
+{
+    private readonly string PG_IMAGE = "jeffrygonzalez/sdt-may-products-empty:latest";
+
+    public IAlbaHost AlbaHost = null!;
+    private readonly PostgreSqlContainer _pgContainer;
+
+    public ProductsDatabaseFixture()
+    {
+        _pgContainer = new PostgreSqlBuilder()
+            .WithDatabase("products_dev")
+            .WithUsername("user")
+            .WithPassword("password")
+            .WithPortBinding(5432, 5432)
+            .WithImage(PG_IMAGE)
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _pgContainer.StartAsync();
+        AlbaHost = await Alba.AlbaHost.For<Program>(builder =>
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddMarten(options =>
+                {
+                    options.Connection(_pgContainer.GetConnectionString());
+                });
+            });
+        });
+    }
+
+    public async Task DisposeAsync()
+    {
+        await AlbaHost.DisposeAsync();
+        await _pgContainer.DisposeAsync();
+    }
+}
