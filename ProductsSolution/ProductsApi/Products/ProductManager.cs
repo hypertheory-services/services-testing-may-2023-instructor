9dc78e5ec3311fe77af0f7308472e2ab9e505c7e
Jeffry Gonzalez <jeff@hypertheory.com>
2023-05-03 12:28:01 -0400

    before lunch for realz

--- ProductsSolution/ProductsApi/Products/ProductManager.cs
+++ ProductsSolution/ProductsApi/Products/ProductManager.cs
@@ -6,11 +6,13 @@ public class ProductManager : IManageTheProductCatalog
 {
     private readonly IGenerateSlugs _slugGenerator;
     private readonly IDocumentSession _session;
+    private readonly IManagePricing _pricingManager;
 
-    public ProductManager(IGenerateSlugs slugGenerator, IDocumentSession session)
+    public ProductManager(IGenerateSlugs slugGenerator, IDocumentSession session, IManagePricing pricingManager)
     {
         _slugGenerator = slugGenerator;
         _session = session;
+        _pricingManager = pricingManager;
     }
 
     public async Task<CreateProductResponse> AddProductAsync(CreateProductRequest request)
@@ -18,16 +20,7 @@ public class ProductManager : IManageTheProductCatalog
         var response = new CreateProductResponse
         {
             Slug = await _slugGenerator.GenerateSlugForAsync(request.Name),
-            Pricing = new ProductPricingInformation
-            {
-                Retail = 42.23M,
-                Wholesale = new ProductPricingWholeInformation
-                {
-                    Wholesale = 40.23M,
-                    MinimumPurchaseRequired = 10
-
-                }
-            }
+            Pricing = await _pricingManager.GetPricingInformationForAsync(request.Supplier)
 
         };
         _session.Insert(response);
